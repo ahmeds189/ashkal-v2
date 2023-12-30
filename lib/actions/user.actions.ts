7f45ce2e677df9bf905d063a98b34faf9e56b503
ahmeds189 <primaryahmedsaid@gmail.com>
2023-12-30 02:25:36 +0200

    fix: varcel deployment error

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,6 +1,6 @@
 'use server'
 import prisma from '@/prisma/db'
-import { User } from '@prisma/client'
+import { type User } from '@prisma/client'
 import { handleError } from '../utils'
 
 export async function createUserAction(userData: User) {
