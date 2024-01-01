64a7ef2845ec19ecea4c62e492d5dd62b5400a69
ahmeds189 <primaryahmedsaid@gmail.com>
2024-01-01 12:21:05 +0200

    styling: new components & ui improvments

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,9 +1,12 @@
-import * as React from "react"
+import * as React from 'react'
 
-import { cn } from "@/lib/utils"
+import { cn } from '@/lib/utils'
 
 export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  icon?: React.ReactNode
+  element?: React.ReactNode
+}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
@@ -11,15 +14,36 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
-          className
+          'flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+          className,
         )}
         ref={ref}
         {...props}
       />
     )
-  }
+  },
 )
-Input.displayName = "Input"
+Input.displayName = 'Input'
 
-export { Input }
+const InputWithElements = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ children, className, type, element, icon, ...props }, ref) => {
+    return (
+      <div className='flex h-10 items-center gap-1 rounded-md border border-input bg-background px-2 ring-offset-background focus-within:ring-2 focus-within:ring-ring focus-within:ring-offset-2 focus-visible:outline-none disabled:cursor-wait'>
+        {icon}
+        <input
+          type={type}
+          className={cn(
+            'flex-1 bg-background text-sm placeholder:text-muted-foreground focus-within:outline-none',
+            className,
+          )}
+          ref={ref}
+          {...props}
+        />
+        {element}
+      </div>
+    )
+  },
+)
+InputWithElements.displayName = 'InputWithElements'
+
+export { Input, InputWithElements }
