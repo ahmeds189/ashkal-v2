f994050fcfdbaa955a139c54adb75ea8694aa83f
ahmeds189 <primaryahmedsaid@gmail.com>
2024-01-01 12:21:47 +0200

    feat: finish schema and forms validation

--- lib/validator.ts
+++ lib/validator.ts
@@ -1,7 +1,20 @@
 import { z } from 'zod'
 
 export const categoryFormSchema = z.object({
-  name: z.string().min(3).max(10),
+  name: z.string().min(3).max(15),
 })
 
 export type categoryFormType = z.infer<typeof categoryFormSchema>
+
+export const productFormSchema = z.object({
+  title: z.string().min(5).max(100),
+  description: z.string().min(10).max(400),
+  imageUrl: z.string().url(),
+  fileUrl: z.string().url(),
+  price: z.string().min(1),
+  isFree: z.boolean().default(false),
+  credit: z.string().optional(),
+  categoryId: z.string().min(1),
+})
+
+export type productFormType = z.infer<typeof productFormSchema>
