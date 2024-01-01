2e37f5ccda9b03eb3f2840391e432546871ba021
ahmeds189 <primaryahmedsaid@gmail.com>
2024-01-01 12:24:07 +0200

    feat: products routes base

--- app/(shop)/products/[id]/edit/page.tsx
+++ app/(shop)/products/[id]/edit/page.tsx
@@ -1,22 +1,20 @@
 import AddProductForm from '@/components/feature/product/form'
 import { auth } from '@clerk/nextjs'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
-export default async function Page() {
+export default function Page() {
   const { userId }: { userId: string | null } = auth()
 
   return (
     <div className='container py-10'>
       <h1 className='pb-2 text-2xl font-bold tracking-tight sm:text-3xl'>
-        Publish a Product
+        Edit a Product
       </h1>
 
       <p className='pb-10 text-sm text-muted-foreground sm:text-base'>
         Please fill all the field below to publish your product.
       </p>
-      <AddProductForm userId={userId} type='create' />
 
-      <ReactQueryDevtools />
+      <AddProductForm userId={userId} type='edit' />
     </div>
   )
 }
