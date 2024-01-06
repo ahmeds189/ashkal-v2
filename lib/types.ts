fd2e268dcdfe708da8f7946b79e86c50993d43b9
ahmeds189 <primaryahmedsaid@gmail.com>
2024-01-06 12:13:10 +0200

    feat: helper functions and finish form schema

--- lib/types.ts
+++ lib/types.ts
@@ -1,4 +1,30 @@
+import { Product } from '@prisma/client'
+
 export type SearchParamProps = {
   params: { id: string }
   searchParams: { [key: string]: string | string[] | undefined }
+}
+
+export type GetAllProductsArgs = {
+  query: string
+  category: string
+  limit: number
+  page: number
+}
+
+export type ExtendedProduct = Product & {
+  category: {
+    id: string
+    name: string
+  }
+  publisher: {
+    id: string
+    username: string
+    firstName: string
+    lastName: string
+    imageUrl: string
+    clerkId: string
+    createdAt: Date
+    updatedAt: Date
+  }
 }
--- lib/validator.ts
+++ lib/validator.ts
@@ -6,13 +6,31 @@ export const categoryFormSchema = z.object({
 export type categoryFormType = z.infer<typeof categoryFormSchema>
 
 export const productFormSchema = z.object({
-  title: z.string().min(5).max(100),
-  description: z.string().min(10).max(400),
-  imageUrl: z.string().url(),
-  fileUrl: z.string().url(),
-  price: z.string().min(1),
+  title: z
+    .string({ required_error: 'Title is required.' })
+    .min(5, 'Title must be at least 5 characters long.')
+    .max(100, 'Title cannot exceed 100 characters.'),
+  description: z
+    .string({ required_error: 'Description is required.' })
+    .min(10, 'Description must be at least 10 characters long.')
+    .max(600, 'Description cannot exceed 600 characters.'),
+  imageUrl: z
+    .string({ required_error: 'Image URL is required.' })
+    .url('Invalid URL format for Image.'),
+  fileUrl: z
+    .string({ required_error: 'File URL is required.' })
+    .url('Invalid URL format for File.'),
+  price: z
+    .number({
+      required_error: 'Price is required.',
+      invalid_type_error: 'Price is required.',
+    })
+    .min(1, 'Price must be at least 1.'),
   isFree: z.boolean().default(false),
-  credit: z.string().optional(),
-  categoryId: z.string().min(1),
+  credit: z.string().url('Invalid URL format for Credit.').optional(),
+  categoryId: z
+    .string({ required_error: 'Category is required.' })
+    .min(1, 'Category must be at least 1 character long.'),
 })
+
 export type productFormType = z.infer<typeof productFormSchema>
